d24203e43ebcbe425702aaca649fd7cf22525807
Necromunger <lil_tommy_tom@hotmail.com>
2024-06-24 21:54:16 +1000

    issue replicated

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,19 @@
 extends Node3D
 
 @export var weapon: Weapon
+@onready var animation_tree = $AnimationTree as AnimationTree
+
+var idle: AnimationNodeAnimation
+var attack: AnimationNodeAnimation
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var root = animation_tree.tree_root as AnimationNodeBlendTree
+	idle = root.get_node("Idle") as AnimationNodeAnimation
+	attack = root.get_node("Attack") as AnimationNodeAnimation
+	
+	print_debug(weapon.idle_anim.resource_name)
+	idle.animation = weapon.idle_anim.resource_name
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
